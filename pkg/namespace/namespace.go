86956ca9568a8f20595d675bec819fda01730c13
Jagjeet Singh <jagjeetsingh7967@gmail.com>
2025-09-28 03:15:09 +0000

    feat: add namespace

--- pkg/namespace/namespace.go
+++ pkg/namespace/namespace.go
@@ -8,11 +8,18 @@ import (
 
 // RunInNewNamespace runs a command in new PID and mount namespace
 func RunInNewNamespace(command []string) error {
-	// create a new namespace using unshare
-	args := []string{"unshare", "--pid", "--mount", "--uts", "--fork"}
-	args = append(args, command...)
+	cmd := exec.Command(
+		"sh",
+		"-c",
+		"mount -t proc proc /proc && exec " + strings.Join(command, " ")
+	)
 
-	cmd := exec.Command(args[0], args[1:]...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWPID |
+			    syscall.CLONE_NEWNS |
+			    syscall.CLONE_NEWUTS,
+	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
